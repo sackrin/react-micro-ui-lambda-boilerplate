6abcaf6c5811465338e41bd8100927096cb72e6a
Hilary Osborne <h.t.osborne@gmail.com>
2020-03-29 18:16:42 +1000

    updated example to use better var names

--- src/Components/ExampleComponent.jsx
+++ src/Components/ExampleComponent.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useCallback } from 'react';
 
-const ExampleComponent = ({ name = 'Neigh' }) => {
+const ExampleComponent = ({ name = 'John Smith' }) => {
   const [thing, setThing] = useState(false);
   const onThing = useCallback(() => {
     setThing(true);
   }, [setThing]);
-	return <div onClick={onThing}>{thing ? 'Typical' : name}</div>;
+	return <div onClick={onThing}>{thing ? 'Clicked!' : name}</div>;
 };
 
 export default ExampleComponent;
